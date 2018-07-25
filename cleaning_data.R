7a6d6315221b4736514e3209fa20bada5564e833
2lsokol2 <36013697+2lsokol2@users.noreply.github.com>
2018-07-25 17:53:30 +0300

    Update cleaning_data.R

--- cleaning_data.R
+++ cleaning_data.R
@@ -27,6 +27,33 @@ print(c(class(x), mode(x), storage.mode(x), typeof(x)))
 
 x <- matrix(cars)
 print(c(class(x), mode(x), storage.mode(x), typeof(x)))
+#dates
+dates <- c("15-9-2009", "16-07-2008", "17 12-2007", "29-02-2011")
+as.POSIXct(dates, format = "%d-%m-%Y")
+'%a Abbreviated weekday name in the current locale. Mon
+%A Full weekday name in the current locale. Monday
+%b Abbreviated month name in the current locale. Sep
+%B Full month name in the current locale. September
+%m Month number (01-12) 09
+%d Day of the month as decimal number (01-31). 28
+%y Year without century (00-99) 13
+%Y Year including century. 2013'
+
+#String normalization
+library(stringr)
+str_trim(" hello world ")
+## [1] "hello world"
+str_trim(" hello world ", side = "left")
+## [1] "hello world "
+str_trim(" hello world ", side = "right")
+## [1] " hello world"
+
+#grep and grepl functions:
+grep("[a-z]", letters) #index
+grepl("[a-z]", letters) #TRUE/FALSE
+
+gender <- c("M", "male ", "Female", "fem.")
+grepl("m", gender, ignore.case = TRUE) #remove sensitive to caps lock
 
 x <- new.env()
 print(c(class(x), mode(x), storage.mode(x), typeof(x)))
