f5e7993c10b15695093329e8b923c121813c236e
2lsokol2 <36013697+2lsokol2@users.noreply.github.com>
2018-08-06 10:28:58 +0300

    Update WOE_TABLE.R

--- WOE_TABLE.R
+++ WOE_TABLE.R
@@ -1,73 +1,115 @@
-#data you should write with "" 
-
-woe_function<-function(data,variable=1){
-  library(ggplot2)
-  library(devtools)
-  library(easyGgplot2)
-  library(plyr)
-  library(knitr)
-  
-file<-read.table(paste0(data,".csv"),sep=",",stringsAsFactors=FALSE,header=TRUE)
-plot0<-ggplot(data = file, aes(x = file[,variable[1]])) + geom_bar() 
-table0<-kable(data.frame(count(t,variable[1])))
-result<-result<-ggplot2.multiplot(plot0, table0, cols=1)
-i<-variable[2]
-while (i<=length(variable)){
-  plot_hist<-ggplot(data = file, aes(x = file[,variable[i]])) + geom_bar() 
-  table_freq<-kable(data.frame(count(t,variable[i])))
-  result<-ggplot2.multiplot(result, plot_hist, table_freq, cols=1)
-  i<-i+1
-  
-} 
- pdf("scoring.pdf") 
-  ggplot2.multiplot(result, plot_hist, table_freq, cols=1)
-  dev.off() }                
-   
-file[,variable[3]]
-variable<-2:10
-              
-                 
- woe_function("woe",2:6)                
-                 
-str(file)
-library(ggplot2)
-library(easyGgplot2)
-library(devtools)
-#install.packages("ggplot")
+ToothGrowth
 #install.packages("ggplot2")
-#install.packages("easyGgplot2")
-#install_github("easyGgplot2", "kassambara")
-#install.packages("devtools")
-#install.packages("knitr")
-#> `geom_smooth()` using method = 'loess'
-#install.packages("easyGgplot2")
-#http://www.sthda.com/english/wiki/ggplot2-multiplot-put-multiple-graphs-on-the-same-page-using-ggplot2
-a<--3:3
-b<-12
-ifelse(a>0,"positive","not positive")
-ifelse(b>0,"positive","not positive")
+library(cowplot)
+library(ggplot2)
+library(gridExtra)
+library(ggpubr)
+setwd("S:/PROJECTS/! Individual/LSokol/!Rstudio/script")
 
-str(t)
+data<-read.csv2("woe.csv",sep=",",dec=".",colClasses = "factor")
+file<-data
+names(file)
+ncol(file)
 
-t<-read.table("woe.csv",sep=",",stringsAsFactors=FALSE,header=TRUE)
-#install.packages("plyr")
-library(plyr)
-library(knitr)
-table<-data.frame(count(t,"rank"))
-kable(table)
+text <- paste("                                                        ",
+              "                                                        ",
+              "                                                        ",
+              "                 Descriptive statistics                              ",
+              "          of potential variables for scoring.",
+              "                                     WOE, IV, distribution, etc.", sep=" ")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+  #generate random target(for this variant)
+  #target<-rbinom(n = 463, size = 1, prob = 0.85)
+  #data<-cbind(target,data)
+  
+  #generate "data" with first column "target" and another columns are sorted alphabetically
+  data<-cbind(data$target,(data[ , sort(names(data[,-which( colnames(data)=="target")]))]))
+  
+  #recall first column,cauth automatically programe calls it "data$target"
+  names(data)[1]<-"target"
+  file<-data
+  
+  plot_list = list()
+  #i - num var
+  i<-1 
+  #second variable is "numplot" - 3 plots for every variable
+  numplot<-1
+  numcol<-ncol(file)
+  for (i in 2:numcol){
+    plot1_hist<-ggplot(file, aes(file[,i])) + 
+      geom_bar(aes(y = (..count..)/sum(..count..)))+
+      scale_y_continuous(labels=scales::percent) +
+      geom_text(aes( y = ((..count..)/sum(..count..)),label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.01) +
+      theme(axis.text.x = element_text(angle=10, vjust=0.9), plot.title = element_text(size = 22, face = "bold",hjust = -0.1, vjust=4), plot.margin = unit(c(1,1,1,1), "cm") ) + 
+      labs( y = "", x = "")+
+      ggtitle(names(file)[i])
+    plot_list[[numplot]] = plot1_hist
+    numplot<-numplot+1
+    
+    
+    #t1-aggregate data for create BR
+    t1<-aggregate(. ~ data[,i], data = data[c(data[,1],data[,i])], 
+                  FUN = function(x) c(all = length(x), bad = sum(x) ) )[,c(1,2)]
+    
+    #counting BR(new columns in aggregate func in R is MATRIX)
+    t2<-transform(t1,br=1-(t1[,2][,2] / t1[,2][,1]))[,c(1,3)]
+    
+    #recall "t2",cauth next action will be join "t2" and "file",
+    #(we must have united column with common name)
+    #"BR" column begins with "WW_"
+    #(all variables soryed by alphabet,we split table by 2 part,with "BR" with WW_" and will be in the right part of table) )
+    names(t2)<-c(names(data)[i],paste0("WW_",names(data)[i]))
+    
+    #add  new column "BR"to our "data" table
+    data<-merge(data,t2,by.x=names(data)[i],by.y=names(t2)[1])
+    
+    str(data)
+    #again sort column,cauth "BR" goes to the end,but joined column goes to the begining,totaly bad column)
+    data<-cbind(data$target,(data[ , sort(names(data[,-which( colnames(data)=="target")]))]))
+    
+    #recall first column,cauth automatically programe calls it "data$target"
+    names(data)[1]<-"target"
+  
+    
+    plot2_hist<-ggplot(data, aes(x=data[,i],y=data[,i+numcol],group=1)) + 
+      geom_line(color="dodgerblue4",size=1)+
+      geom_point() +
+      labs( y = "", x = "")+
+      ylim(c(0, 0.25))
+    plot_list[[numplot]] = plot2_hist
+    numplot<-numplot+1
+    
+    plot3_hist<-ggplot(file, aes(file[,i])) + 
+      geom_bar(aes(y = (..count..)/sum(..count..))) +
+      scale_y_continuous(labels=scales::percent) +
+      geom_text(aes( y = ((..count..)/sum(..count..)),label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.01) +
+      theme(axis.text.x = element_text(angle=10, vjust=0.9), plot.title = element_text(size = 22, face = "bold",hjust = -0.1, vjust=4),plot.margin = unit(c(1,1,1,1), "cm") ) + 
+      labs( y = "", x = "")+
+      ggtitle(names(file)[i])
+    plot_list[[numplot]] = plot3_hist
+    numplot<-numplot+1
+  }
+  
+  
+  pdf("file5.pdf",width=10,height=15,paper='special') 
+  marrangeGrob(plot_list,ncol=1,nrow=3)
+  dev.off()
+  
+  
+  
+  geom_bar(aes(y = ((..count..)/sum(..count..))),stat="identity", width=1.0, 
+           colour = "darkgreen",
+           fill = 'lightslateblue') +
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
