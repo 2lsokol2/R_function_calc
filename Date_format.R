ed7d63a871d102d5b61d0aa7d8b1b75326a8d35f
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-03-25 17:22:33 +0200

    Update Date_format.R

--- Date_format.R
+++ Date_format.R
@@ -1,12 +1,8 @@
-#newtime - csv містить дату у текстовому форматі
-#time - csv містить дату у форматі дати 
-#as.is=TRUE - factor->char(навіть якщо дата у текстовому форматі у csv) vs ( colClasses = "character")
-#head - дозволяє брати тільки "потрібні" клітинки
+
 filedate<-read.csv2("time.csv",sep=";")
 filedate<-head(filedate,50)
 class(filedate$dateone)
 as.Date(file$dateone)
-#as.Date - тільки дати, якщо треба час(навіть часовий пояс і т.д.) - POSIXct,POSIXlt
 
 file<-read.csv2("newtime.csv",sep=";",as.is=TRUE)
 file<-head(file,19)
@@ -23,12 +19,11 @@ library(lubridate)
 
 library(tidyverse)
 library(nycflights13)
-#lubridate ->parse_date_time - розпізнає дату у різних форматах(в orders само вибирає найкращий формат)
+
 parse_date_time("10-31/2010", orders = c("ymd", "dmy", "mdy"))
 parse_date_time(file$dateone, orders = c("ymd", "dmy", "mdy"))
 
 
-#anytime - дозволяє обробляти дату у будь-якому форматі("factor" утворився при імпорті)
 library(RApiDatetime)
 library(anytime)
 anydate("3.145")
@@ -37,7 +32,6 @@ class(sql$Today)
 anydate(sql$Today)
 as.Date(sql$Today)
 
-#не обробляє вектор, також значення із "AM/PM"
 anytime(c("10-11-2011 5:30AM", "16-10-2011 10:10pm"))
 
 #as.POSIXlt == list()
